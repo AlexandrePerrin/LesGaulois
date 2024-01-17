486b2fe4819e76958ceb7cb971a7fc5665fcc4df
AlexandrePerrin <alex.perrin2001@gmail.com>
2024-01-17 08:35:53 +0100

    TP1

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,21 @@
+package personnages;
+
+public class Gaulois {
+	
+	public String nom;
+	public int force;
+
+	// constructeur
+	
+	public Gaulois(String n,int f) {
+		this.nom = n;
+		this.force = f;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	
+
+}
